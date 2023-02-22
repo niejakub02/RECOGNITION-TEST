a7ebcb8ba5dd329542a019266e07df4d843c03fa
niejakub02 <imaslanky@gmail.com>
2023-02-22 18:24:42 +0100

    v2.0

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const canvasList = [];
 const contextList = [];
 let step = -1;
 const resultsLi = document.querySelectorAll('.results-box > span');
-const charactersList = ['話', '夕', '悪', '明', '目', '無', '理', '道', '西', '京'];
+// const charactersList = ['話', '夕', '悪', '明', '目', '無', '理', '道', '西', '京'];
+const charactersList = [
+    '亜', '唖', '娃', '阿', '哀', '愛', '挨', '姶', '逢', '葵', '茜', '穐', '飴', '姐', '虻', '飛', '疱', '靄', '愛', '曖', '靭', '宛', '嵐', '昂', '衿', '股', '胡', '弧', '狐', '玄', '県', '砿', '骨', '亀', '喰', '宮', '恭', '脅', '強', '恒', '洪', '硬', '肯', '侯', '喉', '紅', '荒', '硝', '鴻', '劫', '巨', '拒', '昆', '崑', '献', '矯', '鞠', '砧', '鼓', '粉', '鋼', '誇', '顎', '擬', '犠', '蟻', '祇', '欺', '儀', '義', '妓', '求', '虚', '乾', '喜', '基', '寄', '気', '棋', '稀', '徽', '奇', '軌', '輝', '機', '騎', '技', '宜', '偽', '牛', '具', '愚', '遇', '群', '後', '語', '護', '効', '更', '恒', '洪', '航', '貢', '降', '高', '剛', '豪', '光', '沖', '溝', '鋼', '誇', '幸', '恭', '許', '候', '慌', '坑', '孝', '抗', '皇', '江', '開', '階', '寛', '歓', '汗', '缶', '看', '鉄', '姦', '丸', '岸', '岩', '玩', '翰', '肝', '間', '勘', '寒', '感', '慣', '歓', '棺', '款', '関', '陥', '鬼', '希', '嬉', '吉', '喫', '詰', '却', '拒', '巨', '距', '極', '玉', '勤', '禁', '錦', '仁', '吟', '銀', '琴', '均', '菌', '恨', '倫', '純', '循', '旬', '殉', '淳', '準', '潤', '遵', '儘', '壊', '懐', '拐' 
+    ]
+
 
 // MODEL
 var model = null
@@ -24,7 +28,7 @@ const reposition = (e) => {
 }
 const start = (e) => {
     box.addEventListener("mousemove", draw);
-    
+
     step++;
     const newCanvas = document.createElement('canvas');
     newCanvas.classList.add('input-canvas');
@@ -43,7 +47,8 @@ const stop = () => {
 
 const draw = (e) => {
     contextList[step].beginPath();
-    contextList[step].lineWidth = 20;
+    // contextList[step].filter = "blur(1px)";
+    contextList[step].lineWidth = 16;
     contextList[step].lineCap = "round";
     contextList[step].strokeStyle = "black";
     contextList[step].moveTo(coord.x, coord.y);
@@ -52,26 +57,51 @@ const draw = (e) => {
     contextList[step].stroke();
 }
 
-const saveImage = (e) => {
-    if (contextList.length <= 0 ) return;
+const saveImage = async (e) => {
+    if (contextList.length <= 0) return;
 
     const virtualCanvas = document.createElement('canvas');
-    virtualCanvas.height = 512;
-    virtualCanvas.width = 512;
+    virtualCanvas.height = 64;
+    virtualCanvas.width = 64;
     const virtualCtx = virtualCanvas.getContext('2d');
+    virtualCtx.fillStyle = "black";
+    virtualCtx.fillRect(0, 0, virtualCanvas.width, virtualCanvas.height);
 
     for (let canvas of canvasList) {
-        virtualCtx.drawImage(canvas, 0, 0, 512, 512);
+        virtualCtx.filter = 'invert(1)'
+        virtualCtx.drawImage(canvas, 0, 0, 64, 64);
     }
 
+    // const link = document.createElement('a');
+    // link.download = `${crypto.randomUUID()}.png`;
+    // link.href = virtualCanvas.toDataURL();
+    // link.click();
+
+    let input_image = await tf.browser.fromPixels(virtualCanvas, 1)
+        // .mean(2)
+        // .toFloat()
+        // .expandDims(-1)
+        // .resizeBilinear([64, 64])
+        .asType('float32')
+        // .reshape([64, 64, 1])
+        .div(tf.scalar(255))
+        .array()
+
+        
+        input_image = tf.tensor3d(input_image);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = input_image.shape.width
+    canvas.height = input_image.shape.height
+    await tf.browser.toPixels(input_image, canvas);
     const link = document.createElement('a');
     link.download = `${crypto.randomUUID()}.png`;
-    link.href = virtualCanvas.toDataURL();
+    link.href = canvas.toDataURL();
     link.click();
 }
 
 const clearCanvas = (e) => {
-    if (contextList.length <= 0 ) return;
+    if (contextList.length <= 0) return;
     contextList.pop();
     canvasList.pop().remove();
     step--;
@@ -82,26 +112,36 @@ const predict = async (e) => {
     loadingScreen.style.visibility = 'visible';
 
     const virtualCanvas = document.createElement('canvas');
-    virtualCanvas.height = 512;
-    virtualCanvas.width = 512;
+    virtualCanvas.height = 64;
+    virtualCanvas.width = 64;
     const virtualCtx = virtualCanvas.getContext('2d');
+    virtualCtx.fillStyle = "black";
+    virtualCtx.fillRect(0, 0, virtualCanvas.width, virtualCanvas.height);
 
     for (let canvas of canvasList) {
-        virtualCtx.drawImage(canvas, 0, 0, 512, 512);
+        virtualCtx.filter = 'invert(1)'
+        virtualCtx.drawImage(canvas, 0, 0, 64, 64);
     }
 
-    let image = virtualCtx.getImageData(0, 0, virtualCanvas.height, virtualCanvas.width);
-    let n = []
-    for (let i=3; i<image.data.length; i+=4) {
-        n.push(image.data[i])
-    }
+    let input_image = await tf.browser.fromPixels(virtualCanvas, 1)
+        // .mean(2)
+        // .toFloat()
+        // .expandDims(0)
+        // .expandDims(-1)
+        .asType('float32')
+        // .resizeBilinear([64, 64])
+        .reshape([1, 64, 64, 1])
+        .div(255.0)
 
-    // let input_image = tf.browser.fromPixels(image, 1)
-    let input_image = tf.tensor1d(n);
-    input_image = input_image.asType('float32')
-    input_image = input_image.div(255.0)
-    input_image = input_image.reshape([1, 512, 512])
-    
+    // input_image = tf.tensor4d(input_image);
+
+    // PODGLAD OBRAZKA W 64 X 64 ALE MUSI BYC TENSOR 3D
+    // const link = document.createElement('a');
+    // link.download = `${crypto.randomUUID()}.png`;
+    // link.href = virtualCanvas.toDataURL();
+    // link.click();
+
+
     if (model != null) {
         const result = model.predict(input_image)
         console.log(result.print());
@@ -115,6 +155,7 @@ const predict = async (e) => {
 
         for (let [i, res] of results.entries()) {
             resultsLi[i].innerText = `${res.literal} - ${res.val.toFixed(2)}`
+            if (i === 4) break;
         }
         loadingScreen.style.visibility = 'hidden';
     }
